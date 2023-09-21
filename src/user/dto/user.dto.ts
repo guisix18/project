0ec0d1d103a7e7a8eb606d2aac9e9f4d15e8648a
Enrique Barbosa <e.barbosa@grouplinknetwork.com>
2023-09-21 15:20:52 -0300

    Changed regex password in update userDTO

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -41,8 +41,11 @@ export class UserUpdateDTO {
   @IsEmail(undefined, { message: 'Email is not valid' })
   email: string;
 
-  @IsOptional()
+  @IsString()
   @IsNotEmpty()
   @MinLength(8, { message: 'The password has to be greater or equal than 8' })
+  @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/, {
+    message: 'Password to weak',
+  })
   password: string;
 }
