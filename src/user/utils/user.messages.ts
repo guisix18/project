32d5e306e7b8c5a6c1e3b030c110139cc2e240a2
Guilherme <guilhermeshadow91@gmail.com>
2023-09-21 16:43:17 -0300

    messages to be used in returns

---
+++ src/user/utils/user.messages.ts
@@ -0,0 +1,4 @@
+export const USER_NOT_FOUND = 'User not found';
+export const USER_UPDATED = 'User updated';
+export const USER_EXISTS = 'There is already a user using this email';
+export const USER_DEACTIVATE = 'User deactivate';
