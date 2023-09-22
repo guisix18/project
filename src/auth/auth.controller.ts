4f126ae35c85f9935ee79549a44880aedc72c2ce
Enrique Barbosa <e.barbosa@grouplinknetwork.com>
2023-09-22 16:58:40 -0300

    Auth service, module and controller created

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,15 +8,18 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthRequest } from './models/AuthRequest';
+import { isPublic } from './decorators/is-public.decorator';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @isPublic()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
-  async login(@Req() request) {
-    return this.authService.login();
+  async login(@Req() request: AuthRequest) {
+    return this.authService.login(request.user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { UserDTO } from 'src/user/dto/user.dto';
+import { UserService } from 'src/user/user.service';
+import { UserPayload } from './models/UserPayload';
+import { IUser } from 'src/user/entities/user.interface';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/UserToken';
+import { UnauthorizedError } from './errors/unhauthorized.error';
 
 @Injectable()
 export class AuthService {
-  validateUser(email: string, password: string) {
-    throw new Error('Method not implemented.');
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  login(user: IUser): UserToken {
+    const payload: UserPayload = {
+      sub: user.id,
+      email: user.email,
+      name: user.name,
+      isActive: user.isActive,
+    };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (isPasswordValid) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+    }
+
+    throw new UnauthorizedError(
+      'Email address or password provided is incorrect.',
+    );
   }
 }
