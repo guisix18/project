a6e89fe38dce76b9c2a1195d21a1b75d23da309a
Enrique Barbosa <e.barbosa@grouplinknetwork.com>
2023-09-22 14:08:14 -0300

    Auth controller created

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(LocalAuthGuard)
+  async login(@Req() request) {
+    return this.authService.login();
+  }
+}
