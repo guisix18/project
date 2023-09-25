5593bd57887892fba6c7fd37f6f89e74ae3a40a6
Guilherme <guilhermeshadow91@gmail.com>
2023-09-25 15:23:11 -0300

    service to list tasks and validate query params

--- src/tasks/task.service.ts
+++ src/tasks/task.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { TaskDTO } from './dto/tasks.dto';
-import { Prisma } from '@prisma/client';
+import { Prisma, TaskState } from '@prisma/client';
 import { randomUUID } from 'crypto';
 
 @Injectable()
@@ -27,5 +27,20 @@ export class TasksService {
     });
 
     return task;
+  }
+
+  async listUserTasks(userId: string, query: TaskState): Promise<TaskDTO[]> {
+    const validate = Object.keys(TaskState).includes(query);
+
+    const tasks = await this.prisma.task.findMany({
+      where: {
+        userId,
+        AND: {
+          progress: validate ? query : undefined,
+        },
+      },
+    });
+
+    return tasks;
   }
 }
