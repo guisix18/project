4e7db6560df875cebc885271665dda0e63138db7
Guilherme <guilhermeshadow91@gmail.com>
2023-09-26 17:16:57 -0300

    controllers methods for forget and new password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,21 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserDTO } from './dto/user.dto';
-import { Prisma, User } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 import { randomUUID } from 'crypto';
 import * as bcrypt from 'bcrypt';
 import { select } from './utils/user.select';
 import { UserPayload } from 'src/auth/models/UserPayload';
 import { JwtService } from '@nestjs/jwt';
-import Mailgen from 'mailgen';
-import { MailOptions } from 'nodemailer/lib/json-transport';
-import { SendMailOptions } from 'nodemailer';
+import { urlGen } from './utils/user.urlgen';
+import { MailerService } from '@nestjs-modules/mailer';
+import { EXPIRED_TOKEN, USER_NOT_FOUND } from './utils/user.messages';
+import { Request } from 'express';
+import { Content } from 'mailgen';
+const Mailgen = require('mailgen');
 
 @Injectable()
 export class UserService {
   constructor(
     private readonly prisma: PrismaService,
     private readonly jwtService: JwtService,
+    private readonly mailer: MailerService,
   ) {}
 
   async getAllUsers(): Promise<UserDTO[]> {
@@ -43,6 +47,7 @@ export class UserService {
       name: userData.name,
       email: userData.email,
       password: bcrypt.hashSync(userData.password, 10),
+      createdAt: new Date(),
     };
 
     const user = await this.prisma.user.create({
@@ -84,33 +89,53 @@ export class UserService {
     return;
   }
 
-  private createEmailBody(user: UserDTO) {
+  private createEmailBody(user: UserDTO, req: Request) {
     const mailGen = new Mailgen({
       theme: 'default',
       product: {
         name: 'Reset Password',
-        link: '',
+        link: req.protocol + req.get('Host'),
       },
     });
 
-    const emailBody = {
+    const link = urlGen(req).replaceAll('forget-password', 'new-password');
+
+    const emailBody: Content = {
       body: {
         name: user.name,
         intro: 'Reset your password',
         action: {
           instructions:
             'Para que seja possível o reset de senha, por favor, clique no botão abaixo e escreva sua nova senha',
-            button: {
-              color: '#0099FF',
-              text: 'Reset your password here',
-              link: 
-            }
+          button: {
+            color: '#0099FF',
+            text: 'Reset your password here',
+            link: `${link}?token=${user.resetToken}`,
+          },
         },
+        outro: 'Need help? Please, send me a email',
       },
     };
+
+    const email = mailGen.generate(emailBody);
+
+    return email;
   }
 
-  async resetPassword(email: string): Promise<void> {
+  async sendMail(user: UserDTO, req: Request): Promise<void> {
+    const htmlToBeSented = this.createEmailBody(user, req);
+
+    await this.mailer.sendMail({
+      to: user.email,
+      from: 'guisix16@gmail.com',
+      subject: 'Reset your password',
+      html: htmlToBeSented,
+    });
+
+    return;
+  }
+
+  async forgetPassword(request: Request, email: string): Promise<void> {
     const user = await this.prisma.user.findFirst({
       where: {
         email,
@@ -118,10 +143,11 @@ export class UserService {
     });
 
     const userPayload: UserPayload = {
+      id: user.id,
+      sub: user.id,
       email: user.email,
       name: user.name,
       isActive: user.isActive,
-      sub: user.id,
     };
 
     const token = await this.jwtService.signAsync(userPayload);
@@ -135,6 +161,40 @@ export class UserService {
       },
     });
 
-    await this.createEmailBody(userWithToken);
+    await this.sendMail(userWithToken, request);
+
+    return;
+  }
+
+  async newPassword(token: string, password: string): Promise<UserDTO> {
+    try {
+      const decodedToken = this.jwtService.verify(token);
+
+      const user = await this.prisma.user.findFirst({
+        where: {
+          id: decodedToken.id,
+        },
+      });
+
+      if (!user) throw new HttpException(USER_NOT_FOUND, 404);
+
+      const userWithNewPass = await this.prisma.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          password: bcrypt.hashSync(password, 10),
+          updatedAt: new Date(),
+          resetToken: null,
+        },
+        select,
+      });
+
+      return userWithNewPass;
+    } catch (err) {
+      if (err) {
+        throw new HttpException(EXPIRED_TOKEN, 400);
+      }
+    }
   }
 }
