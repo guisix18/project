a784f8b40e5296261cb2ad71156834fd3810f7e6
Enrique Barbosa <e.barbosa@grouplinknetwork.com>
2023-10-20 14:22:52 -0300

    Patch and Delete Controller created

--- src/group/group.controller.ts
+++ src/group/group.controller.ts
@@ -1,13 +1,17 @@
 import {
   Body,
   Controller,
+  Delete,
+  Get,
   HttpCode,
   HttpStatus,
+  Param,
+  Patch,
   Post,
   Res,
 } from '@nestjs/common';
-import { GroupDTO } from './dto/group.dto';
-import { Response } from 'express';
+import { GroupDTO, UpdateGroupDTO } from './dto/group.dto';
+import { Response, response } from 'express';
 import { GroupService } from './group.service';
 
 @Controller('group')
@@ -23,5 +27,31 @@ export class GroupController {
     const createdGroup = await this.groupServices.createGroup(data);
 
     return response.json(createdGroup);
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async listGroups(@Res() response: Response): Promise<Response<GroupDTO[]>> {
+    const groups = await this.groupServices.listGroups();
+
+    return response.json(groups);
+  }
+
+  @Patch('/:groupId')
+  @HttpCode(HttpStatus.ACCEPTED)
+  async updateGroup(
+    @Param('groupId') groupId: string,
+    @Res() response: Response,
+    @Body() data: UpdateGroupDTO,
+  ) {
+    const updatedGroup = await this.groupServices.updateGroup(data, groupId);
+
+    return response.json(updatedGroup);
+  }
+
+  @Delete('/:groupId')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteGroup(@Param('groupId') groupId: string): Promise<void> {
+    return await this.groupServices.deleteGroup(groupId);
   }
 }
