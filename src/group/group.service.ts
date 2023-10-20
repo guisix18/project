a09bfc3165c63d5acf6c4f04e0264b8963aae097
Enrique Barbosa <e.barbosa@grouplinknetwork.com>
2023-10-20 14:23:16 -0300

    Patch and Delete Services created

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { GroupDTO } from './dto/group.dto';
+import { GroupDTO, UpdateGroupDTO } from './dto/group.dto';
 import { Prisma } from '@prisma/client';
 import { randomUUID } from 'crypto';
+import { select } from './utils/group.select';
 
 @Injectable()
 export class GroupService {
@@ -19,5 +20,37 @@ export class GroupService {
     });
 
     return group;
+  }
+
+  async listGroups(): Promise<GroupDTO[]> {
+    const groups = await this.prisma.groupTask.findMany({
+      select,
+    });
+
+    return groups;
+  }
+
+  async updateGroup(
+    data: UpdateGroupDTO,
+    groupId: string,
+  ): Promise<UpdateGroupDTO> {
+    const groupUpdated = await this.prisma.groupTask.update({
+      where: {
+        id: groupId,
+      },
+      data,
+    });
+
+    return groupUpdated;
+  }
+
+  async deleteGroup(groupId: string): Promise<void> {
+    await this.prisma.groupTask.delete({
+      where: {
+        id: groupId,
+      },
+    });
+
+    return;
   }
 }
